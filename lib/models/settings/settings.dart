0680a1bebd85dd817fdf972c8cd3ab67188bcc7b
yoonj4 <justin.o.yoon@gmail.com>
2020-10-26 14:13:09 -0700

    Added threat meter thresholds to setting object.

--- lib/models/settings/settings.dart
+++ lib/models/settings/settings.dart
@@ -1,9 +1,30 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:the_yagi_project/models/settings/message_template.dart';
 
 class Settings {
+  MessageTemplate messageTemplate;
+  double warningValue;
+  double alertValue;
+
   Settings() {
     messageTemplate = new MessageTemplate();
+    _initThresholdValues()
   }
-  MessageTemplate messageTemplate;
+
+  void _initThresholdValues() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    warningValue = prefs.getDouble("warningValue");
+    if (warningValue == null) {
+      warningValue = 0.33;
+      prefs.setDouble("warningValue", warningValue);
+    }
+
+    alertValue = prefs.getDouble("alertValue");
+    if (alertValue == null) {
+      alertValue = 0.66;
+      prefs.setDouble("alertValue", alertValue);
+    }
+  }
 }
 
